c7e92460efcd980ca2332c31f1bd9251dbccf793
himasai_1423 <bhsr2003@gmail.com>
2022-06-19 11:30:46 +0530

    created input box and fetched result from TVMAZE api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPagelayout from '../components/MainPagelayout';
 
 const Home = () => {
-  return <MainPagelayout>This is home</MainPagelayout>;
+  const [input, setInput] = useState('');
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPagelayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPagelayout>
+  );
 };
 
 export default Home;
