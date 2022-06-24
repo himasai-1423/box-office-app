44635ef9f59e7a12cd4e2823258778df3160dfc9
himasai_1423 <bhsr2003@gmail.com>
2022-06-24 09:49:01 +0530

    Enabling PWA

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+// import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -17,4 +19,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+serviceWorkerRegistration.register();
